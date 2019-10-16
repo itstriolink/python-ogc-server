aa589d13f42e9e39cc77b0eda2511a22acf680d3
labian <labiangashi@gmail.com>
2019-10-16 07:55:47 +0200

    Starting to get used to Python (Also importing some important libraries for the server)

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-# Test commit from PyCharm # 3
+import geojson
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Student(BaseModel):
+    name: str
+    year: int
+    is_master: bool = False
+
+
+@app.get("/")
+def index():
+    return "This is the index page!"
+
+
+@app.get("/hello")
+def hello_world():
+    return "Hello " + "World"
+
+
+@app.get("/students")
+def students():
+    return "Students"
+
+
+@app.get("/students/{student_id}")
+def get_student(student_id: int, q: str = None):
+    return {"student_id": student_id, "q": q}
+
+
+@app.put("/students/{student_id}")
+def save_student(student_id: int, student: Student):
+    return {"student_name": student.name, "student_id": student_id}
+
