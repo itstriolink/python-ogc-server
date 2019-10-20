d65e1eb3195a5aeaf46966334477578864927cb8
labiangashi <labiangashi@gmail.com>
2019-10-20 15:55:30 +0200

    Removing some useless methods in the main file that were used just as a demonstration of FastApi

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import geojson
 from fastapi import FastAPI
-from pydantic import BaseModel
+from collections import make_index
 
 app = FastAPI()
 
 
-class Student(BaseModel):
-    name: str
-    year: int
-    is_master: bool = False
+def main():
+    make_index()
 
 
 @app.get("/")
@@ -16,28 +13,13 @@ def index():
     return "This is the index page!"
 
 
-@app.get("/hello")
+@app.get("/hello/world")
 def hello_world():
     return "Hello " + "World"
 
 
-@app.get("/students")
-def students():
-    return "Students"
-
-
-@app.get("/students/{student_id}")
-def get_student(student_id: int, q: str = None):
-    return {"student_id": student_id, "q": q}
-
-
-@app.put("/students/{student_id}")
-def save_student(student_id: int, student: Student):
-    return {"student_name": student.name, "student_id": student_id}
-
-
 @app.get("/tiles/castles/{x}/{y}/{z}")
-def raster_tile(x: int, y: int, z: int):
+def get_raster_tile(x: int, y: int, z: int):
     return {"x": x, "y": y, "z": z}
 
 
@@ -45,3 +27,6 @@ def raster_tile(x: int, y: int, z: int):
 def get_castles(bbox: str, limit: int = None):
     bounding_box = bbox.split(",")
     return {"bbox": bounding_box, "limit": limit}
+
+
+if __name__ == "__main__": main()
