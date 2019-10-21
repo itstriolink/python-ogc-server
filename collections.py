a3bffb66b764791debbbbe8b532756e0745b7690
labiangashi <labiangashi@gmail.com>
2019-10-21 14:36:14 +0200

    Some more changes to the collections file

--- collections.py
+++ collections.py
@@ -1,7 +1,9 @@
 import time
-from tiles import TileCache
-from os import tmpfile
 import geojson
+import s2sphere
+from tiles import TileCache
+import os
+import io
 from pydantic import BaseModel
 
 
@@ -14,10 +16,10 @@ class CollectionMetadata(BaseModel):
 class Collection(BaseModel):
     metadata: CollectionMetadata
     tile_cache: TileCache
-    data_file: tmpfile()  # not sure yet
+    data_file: io.FileIO  # not sure yet
     offset: list[int]
-    bbox: list[geojson.Rect]  # Can't find it yet
-    web_mercator: list[geojson.Point]
+    bbox: list[s2sphere.LatLngRect]  # Can't find it yet
+    web_mercator: list[s2sphere.Point]
     id: list[str]
     by_id: dict()
 
@@ -27,7 +29,8 @@ class Index(BaseModel):
     public_path: str
 
 
-def make_index(collections, public_path):
+def make_index(collections: dict, public_path):
+    index = Index(collections=dict[str, Collection], public_path=public_path)
     pass
 
 
@@ -55,3 +58,33 @@ def get_tile(collection, zoom, x, y):
 
 def get_collection_metadata(path):
     return None
+
+
+def read_collection(name, path, if_modified_since):
+    abs_path, err = os.path.abspath(path)
+
+    stat = os.stat(abs_path)
+
+    with open(abs_path, "r") as file:
+        data = file.read()
+
+    coll = Collection()
+
+    coll.metadata.last_modified = stat.st_mtime
+    coll.metadata.name = name
+    coll.metadata.path = abs_path
+
+    features = geojson.FeatureCollection
+
+    features = geojson.loads(data)
+
+    data_file = os.tmpfile("", "miniwfs-*.geojson")
+
+    coll.data_file = data_file
+
+    num_features = len(features.items())
+    coll.bbox = dict(list[s2sphere.LatLngRect], num_features)
+    coll.id = dict(list[str], num_features)
+    coll.web_mercator = dict(s2sphere.Point, num_features)
+    coll.offset = dict()
+    coll.by_id = dict()
