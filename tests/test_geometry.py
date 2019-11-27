69abf129c5af1c0ab3cfed2077309df81378346d
labiangashi <labiangashi@gmail.com>
2019-11-27 19:39:28 +0100

    Fixing the test data and also adding some test cases

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -21,12 +21,18 @@ class TestGeometry:
 
     def test_get_tile_bounds(self):
         bounding_box = wfs_server.geometry.encode_bbox(wfs_server.geometry.get_tile_bounds(12, 2148, 1436))
-        # TODO
+        expected_bbox = [8.789062, 47.219568, 8.876953, 47.279229]
+
+        for index, edge in enumerate(bounding_box):
+            if not round(edge, 6) == expected_bbox[index]:
+                assert False
+
+        assert True
 
     def test_project_web_mercator(self):
         received = wfs_server.geometry.project_web_mercator(s2sphere.LatLng.from_degrees(41.850, -87.650))
-        expected = Point(65.67111111111113, 95.17492654697409)
+        expected = Point(65.671111111111113, 95.17492654697409)
 
         delta = received.__sub__(expected)
 
-        # self.assertGreater(math.fabs(delta.x) + math.fabs(delta.y), 1e-9)
+        # assert math.fabs(delta.x)+math.fabs(delta.y) > 1e-9
--- tests/test_index.py
+++ tests/test_index.py
@@ -4,34 +4,49 @@ import os.path
 import s2sphere
 
 import wfs_server.index
-from wfs_server.data_structures import APIResponse
+from wfs_server.data_structures import HTTP_RESPONSES
 
 
 def create_test_index():
-    collection_castles = os.path.join("test_data", "castles.geojson")
-    collection_lakes = os.path.join("test_data", "lakes.geojson")
-    public_path = r"https://test.example.org/wfs/"
+    collection_castles = os.path.join("tests", "test_data", "castles.geojson")
+    collection_lakes = os.path.join("tests", "test_data", "lakes.geojson")
 
+    public_path = r"https://test.example.org/wfs/"
     index = wfs_server.index.make_index({"castles": collection_castles, "lakes": collection_lakes}, public_path)
 
     return index
 
 
-class TestIndex():
-    # def test_get_items_empty_bbox(self):
-    #    index = create_test_index(self)
-    #    items, _, _ = get_items(index, "castles", '', 0, 100, s2sphere.LatLngRect)
-    #    TODO
-    pass
+class TestIndex:
+    def test_get_items_empty_bbox(self):
+        index = create_test_index()
+        response = get_items(index, "castles", 100, s2sphere.LatLngRect())
+
+        # assert response.content == ""
+
+    def test_get_item_existing_item(self):
+        index = create_test_index()
+        received = index.get_item("castles", "W418392510")
+
+        assert received.content is not None and received.content.properties["name"] == "Castello Scaligero"
+
+    def test_get_item_no_such_collection(self):
+        index = create_test_index()
+        received = index.get_item("no-such-collection", "123")
+
+        assert received.http_response is not None and received.http_response == HTTP_RESPONSES["NOT_FOUND"]
+
+    def test_get_item_no_such_item(self):
+        index = create_test_index()
+        received = index.get_item("castles", "no-such-feature-id")
+
+        assert received.http_response is not None and received.http_response == HTTP_RESPONSES["NOT_FOUND"]
 
 
-def get_items(index: wfs_server.index.Index, collection: str, start_id: str, start_index: int, limit: int,
+def get_items(index: wfs_server.index.Index, collection: str, limit: int,
               bounding_box: s2sphere.LatLngRect):
     writer = io.BytesIO()
 
-    response = index.get_items(collection, start_id, start_index, limit, bounding_box, writer)
+    response = index.get_items(collection, limit, bounding_box, writer)
 
-    if response.http_response is not None:
-        return APIResponse(None, response.http_response)
-
-    return APIResponse(response.content, response.http_response)
+    return response
